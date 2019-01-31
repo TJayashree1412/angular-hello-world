093db7e7d833651c294bba1cccac33fb5a984791
jaythot1 <jaythot1@in.ibm.com>
2019-01-31 14:30:20 +0530

    initial

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,11 +3,16 @@ import { template } from '@angular/core/src/render3';
 
 @Component({
     selector: 'courses',
-    template: '<h2>{{ getTitle() }}</h2>'
+    template: `
+        <h2>{{ getTitle() }}</h2>
+        <ul>
+            <li *ngFor="let course of courses">
+                {{ course }}
+            </li>
+        </ul>
+        `
 })
 export class CoursesComponent{
     title = "List of courses";
-    getTitle(){
-        return this.title;
-    }
+    courses = ["courses1","courses2","courses3"];
 }
