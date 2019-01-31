17ecce7f5c0c12e905911e6bd145409042e20ea8
jaythot1 <jaythot1@in.ibm.com>
2019-01-31 13:33:42 +0530

    update

---
+++ src/app/courses.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { template } from '@angular/core/src/render3';
+
+@Component({
+    selector: 'courses',
+    template: '<h2>courses</h2>'
+})
+export class CoursesComponent{
+
+}
